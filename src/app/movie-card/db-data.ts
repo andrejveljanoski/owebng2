69c3a6e3cb17adb60add6c7772723c74feffe658
andrejveljanoski <andrejveljanoskif1@gmail.com>
2024-02-23 19:18:04 +0100

    Images renamed2

--- src/app/movie-card/db-data.ts
+++ src/app/movie-card/db-data.ts
@@ -6,7 +6,7 @@ export const MOVIES: Movie[] = [
     year: 1972,
     genre: 'Crime',
     rating: 9.2,
-    image: '/images/godfather.avif',
+    image: '../../assets/images/godfather.avif',
     description:
       'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
   },
@@ -16,7 +16,7 @@ export const MOVIES: Movie[] = [
     year: 1994,
     genre: 'Drama',
     rating: 9.3,
-    image: 'imasges/shawshank.jpg',
+    image: '../../assets/images/shawshank.jpg',
     description:
       'Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner.                                         ',
   },
@@ -26,7 +26,7 @@ export const MOVIES: Movie[] = [
     year: 2008,
     genre: 'Action',
     rating: 9,
-    image: '/images/darkknight.jpg',
+    image: '../../assets/images/darkknight.jpg',
     description:
       'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
   },
@@ -36,7 +36,7 @@ export const MOVIES: Movie[] = [
     year: 1998,
     genre: 'Comedy',
     rating: 7,
-    image: '/images/rushhour.jpg',
+    image: '../../assets/images/rush.jpg',
     description:
       'Detective Inspector Lee, a loyal Hong Kong inspector, teams up with Detective James Carter, a loudmouthed L.A.P.D. officer. Their mission: to rescue the kidnapped daughter of the Chinese Consul while trying to apprehend a dangerous crime lord.',
   },
@@ -46,7 +46,7 @@ export const MOVIES: Movie[] = [
     year: 1978,
     genre: 'Musical',
     rating: 7.2,
-    image: '/images/grease.jpg',
+    image: '../../assets/images/grease.jpg',
     description:
       "Sally's love story with Danny Zuko, the leader of the T-Birds, a group of black-jacket greasers while discovering her new self.",
   },
